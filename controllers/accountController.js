fdca6ce40a0f7cc109084b1a4e8a02bfc149740e
iriebijudicael <iriejudicaelelvis225@gmail.com>
2026-02-05 09:34:26 +0000

    login & Registration issue view fixed for displaying

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,14 +4,16 @@ const utilities = require("../utilities");
 
 
 /* ****************************************
-*  Deliver login view
-* *************************************** */
+ *  Deliver login view
+ * *************************************** */
 async function buildLogin(req, res, next) {
-  let nav = await utilities.getNav()
+  let nav = await utilities.getNav();
+  // req.flash("notice", "This is a flash message.!!!@2")
   res.render("account/login", {
     title: "Login",
+    errors: null,
     nav,
-  })
+  });
 }
 
 /* ****************************************
